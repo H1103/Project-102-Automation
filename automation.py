24e2cfd65dcd1eda895a2761e7812e35f1f23d5f
Hartej Nayar <71260380+H1103@users.noreply.github.com>
2021-08-04 23:29:25 +0530

    Update automation.py

--- automation.py
+++ automation.py
@@ -3,37 +3,6 @@ import time
 import dropbox
 import random
 
-start_time = time.time()
-
-def take_snapshot():
-    number = random.randint(0, 100)
-    videoCaptureObject = cv2.VideoCapture(0)
-    result= True
-    while(result):
-        ret, frame = videoCaptureObject.read()
-        img_name = "img" + str(number) + ".png"
-        cv2.imwrite(img_name, frame)
-        result = False
-        return img_name
-    videoCaptureObject.release()
-    cv2.destroyAllWindows()
-
-def upload_file(img_name):
-    access_token = "kqQnDU-9crcAAAAAAAAAAcdSJ8FDBRjdYRnblnywXsgGwDYiLDvkDr4gbxmRoF1p"
-    file = img_name
-    file_from = file
-    file_to = "/newFolder1/" + img_name
-    dbx = dropbox.Dropbox(access_token)
-    with open(file_from, 'rb') as f:
-        dbx.files_upload(f.read(), file_to, mode = dropbox.files.WriteMode.overwrite)
-
-def main():
-    while(True):
-        if((time.time()- start_time)>=120):
-            name= take_snapshot()
-            upload_file(name)
-
-# Calculator part
 print("Calculator + Picture")
 print("get your picture while running this program after every 2 minutes. Same time do your calculation!")
 print(' 1 To Add')
